96f0efac782168461be62f36bd78f1c8c3ea76e2
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-01-21 12:08:03 +0100

    Ajout des LEDs dans le projet StopWatch

--- src/vhdl/StopWatch/StopWatch.vhd
+++ src/vhdl/StopWatch/StopWatch.vhd
@@ -7,6 +7,7 @@ entity StopWatch is
         clk_i             : in  std_logic;
         btn_center_i      : in  std_logic;
         switches_i        : in  std_logic_vector(15 downto 0);
+        leds_o            : out std_logic_vector(15 downto 0);
         disp_segments_n_o : out std_logic_vector(0 to 6);
         disp_point_n_o    : out std_logic;
         disp_select_n_o   : out std_logic_vector(3 downto 0)
@@ -17,4 +18,6 @@ architecture Structural of StopWatch is
     -- Declarations
 begin
     -- Concurrent statements
+
+    leds_o <= "0000000000000000";
 end Structural;
