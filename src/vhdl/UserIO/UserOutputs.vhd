22809cbb0f85780ecf23e591bc3857ed946458f9
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-04-06 14:47:52 +0200

    Ajout du module UserIO

---
+++ src/vhdl/UserIO/UserOutputs.vhd
@@ -0,0 +1,18 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity UserOutputs is
+    generic(
+        -- Declarations
+    );
+    port(
+        -- Declarations
+    );
+end UserOutputs;
+
+architecture Behavioral of UserOutputs is
+    -- Declarations
+begin
+    -- Concurrent statements
+end Behavioral;
