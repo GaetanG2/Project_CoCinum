8c69ead26ad80b23711cf0ecfadcf11362613d17
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-04-06 14:59:06 +0200

    Ajout de l'entité UserInputs

---
+++ src/vhdl/UserIO/UserInputs.vhd
@@ -0,0 +1,20 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.Virgule_pkg.all;
+
+entity UserInputs is
+    generic(
+        -- Declarations
+    );
+    port(
+        -- Declarations
+    );
+end UserInputs;
+
+architecture Behavioral of UserInputs is
+    -- Declarations
+begin
+    -- Concurrent statements
+end Behavioral;
