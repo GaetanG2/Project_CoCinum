14352f0c35162b67cf1b932990e8a1fd837b2c74
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-04-08 16:53:36 +0200

    I2C : correction du script de simulation

--- src/vhdl/I2C/tests/I2CMasterTestbench.tcl
+++ src/vhdl/I2C/tests/I2CMasterTestbench.tcl
@@ -4,14 +4,14 @@ if {[current_sim] ne ""} {
     close_sim -force
 }
 
-set_property top I2CTestbench [get_filesets sim_1]
+set_property top I2CMasterTestbench [get_filesets sim_1]
 
 # Start the simulator.
 launch_simulation -simset sim_1 -mode behavioral
 restart
 
 # Add waves for signals in I2STransmitter.
-set tbdiv [add_wave_divider -before_wave clk_sim I2CTestbench]
+set tbdiv [add_wave_divider -before_wave clk_sim I2CMasterTestbench]
 set div   [add_wave_divider -before_wave $tbdiv  I2CMaster]
 add_wave -after_wave $div master_inst/*
 
