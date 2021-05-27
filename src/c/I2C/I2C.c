5ed5fbc409b59d7bebf1945cd9d1dcc1aa8b4e6d
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-05-27 17:37:51 +0200

    Ajout du pilote I2C

---
+++ src/c/I2C/I2C.c
@@ -0,0 +1,39 @@
+
+#include "I2C.h"
+
+void I2CMaster_init(I2CMaster *dev) {
+    InterruptController_disable(dev->intc, dev->evt_mask);
+    InterruptController_clear_events(dev->intc, dev->evt_mask);
+}
+
+void I2CMaster_send_receive(I2CMaster *dev, uint8_t slave_address, uint8_t send_len, uint8_t recv_len, uint8_t *data) {
+    *dev->control = (send_len << 12) | (recv_len << 8) | slave_address;
+
+    uint32_t buf = 0;
+    for (int n = 0; n < 4; n ++) {
+        if (n < send_len) {
+            buf |= data[n];
+        }
+        buff <<= 8;
+    }
+    *dev->data = buf;
+
+    while (!InterruptController_has_events(dev->intc, dev->evt_mask));
+    InterruptController_clear_events(dev->intc, dev->evt_mask);
+
+    buf = *dev->data;
+    for (int n = 3; n >= 0; n --) {
+        if (n < recv_len) {
+            data[n] = buf & 0xFF;
+        }
+        buf >>= 8;
+    }
+}
+
+void I2CDevice_init(I2CDevice *dev) {
+    I2CMaster_init(dev->i2c);
+}
+
+void I2CDevice_send_receive(I2CDevice *dev, uint8_t send_len, uint8_t recv_len, uint8_t *data) {
+    I2CMaster_send_receive(dev->i2c, dev->slave_address, send_len, recv_len, data);
+}
---
+++ src/c/I2C/I2C.h
@@ -0,0 +1,30 @@
+
+#ifndef I2C_H_
+#define I2C_H_
+
+#include <InterruptController/InterruptController.h>
+
+enum {
+    I2C_DATA_REG,
+    I2C_CONTROL_REG
+} I2CMasterRegs;
+
+typedef struct {
+    InterruptController *intc;
+    uint32_t evt_mask;
+    volatile uint32_t *data;
+    uint32_t *control;
+} I2CMaster;
+
+void I2CMaster_init(I2CMaster *dev);
+void I2CMaster_send_receive(I2CMaster *dev, uint8_t slave_address, uint8_t send_len, uint8_t recv_len, uint8_t *data);
+
+typedef struct {
+    I2CMaster * i2c;
+    uint8_t slave_address;
+} I2CDevice;
+
+void I2CDevice_init(I2CDevice *dev);
+void I2CDevice_send_receive(I2CMaster *dev, uint8_t send_len, uint8_t recv_len, uint8_t *data);
+
+#endif
