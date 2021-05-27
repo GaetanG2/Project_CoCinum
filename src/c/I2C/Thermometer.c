97a3aade989f2cae0fb0d53542eab4f164447d4b
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-05-27 18:27:59 +0200

    I2C : initialiser les données à transmettre

--- src/c/I2C/Thermometer.c
+++ src/c/I2C/Thermometer.c
@@ -6,7 +6,7 @@ void Thermometer_init(I2CDevice *dev) {
 }
 
 int16_t Thermometer_get_temperature(I2CDevice *dev) {
-    uint8_t data[2];
+    uint8_t data[2] = {0, 0};
     I2CDevice_send_receive(dev, 1, 2, data);
     return ((int16_t)data[0] << 5) | (data[1] >> 3);
 }
