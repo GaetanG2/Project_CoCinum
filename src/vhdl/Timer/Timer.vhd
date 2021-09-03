0cdb183bfc091743b60eae9944087ce6f33a44d5
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-09-03 15:00:47 +0200

    Mise à jour Timer

--- src/vhdl/Timer/Timer.vhd
+++ src/vhdl/Timer/Timer.vhd
@@ -9,24 +9,26 @@ entity Timer is
     port(
         clk_i     : in  std_logic;
         reset_i   : in  std_logic;
+        valid_i   : in  std_logic;
+        ready_o   : out std_logic;
         address_i : in  std_logic;
-        write_i   : in  std_logic;
-        data_i    : in  word_t;
-        data_o    : out word_t;
-        irq_o     : out std_logic
+        rdata_o   : out word_t;
+        wdata_i   : in  word_t;
+        write_i   : in  std_logic_vector(3 downto 0);
+        evt_o     : out std_logic
     );
 end Timer;
 
 architecture Behavioral of Timer is
-    signal limit_reg, count_reg : unsigned(31 downto 0);
+    signal limit_reg, count_reg : unsigned_word_t;
 begin
     p_limit_reg : process(clk_i)
     begin
         if rising_edge(clk_i) then
             if reset_i = '1' then
-                limit_reg <= to_unsigned(0, limit_reg'length);
-            elsif write_i = '1' and address_i = '0' then
-                limit_reg <= unsigned(data_i);
+                limit_reg <= unsigned(WORD0);
+            elsif valid_i = '1' and address_i = '0' then
+                update_unsigned_word(limit_reg, wdata_i, write_i);
             end if;
         end if;
     end process p_limit_reg;
@@ -35,18 +37,19 @@ begin
     begin
         if rising_edge(clk_i) then
             if reset_i = '1' then
-                count_reg <= to_unsigned(0, count_reg'length);
-            elsif write_i = '1' and address_i = '1' then
-                count_reg <= unsigned(data_i);
+                count_reg <= unsigned(WORD0);
+            elsif valid_i = '1' and address_i = '1' and write_i /= "0000" then
+                update_unsigned_word(count_reg, wdata_i, write_i);
             elsif count_reg >= limit_reg then
-                count_reg <= to_unsigned(0, count_reg'length);
+                count_reg <= unsigned(WORD0);
             else
                 count_reg <= count_reg + 1;
             end if;
         end if;
     end process p_count_reg;
 
-    data_o <= word_t(limit_reg) when address_i = '0' else
-              word_t(count_reg);
-    irq_o  <= '1' when count_reg >= limit_reg and limit_reg > 0 else '0';
+    rdata_o <= word_t(limit_reg) when address_i = '0' else
+               word_t(count_reg);
+    evt_o   <= '1' when count_reg >= limit_reg and limit_reg > 0 else '0';
+    ready_o <= valid_i;
 end Behavioral;
