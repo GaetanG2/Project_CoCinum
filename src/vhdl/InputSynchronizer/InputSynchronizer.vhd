3b90762aa4f3c9113b66fefed605d52e6cd879a2
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-09-03 14:55:27 +0200

    InputSynchronizer : Compatibilité avec GHDL

--- src/vhdl/InputSynchronizer/InputSynchronizer.vhd
+++ src/vhdl/InputSynchronizer/InputSynchronizer.vhd
@@ -20,21 +20,24 @@ entity InputSynchronizer is
 end InputSynchronizer;
 
 architecture Behavioral of InputSynchronizer is
-    -- A intermediate register that will synchronize data_i.
-    signal data_reg : std_logic_vector(WIDTH - 1 downto 0);
+    -- Intermediate registers that will synchronize data_i.
+    signal data0_reg : std_logic_vector(WIDTH - 1 downto 0);
+    signal data1_reg : std_logic_vector(WIDTH - 1 downto 0);
 
     -- The ASYNC_REG attribute informs Xilinx Vivado that each flip-flop pair
     -- data_reg(i)/data_o(i) should be placed in the same slice.
     attribute ASYNC_REG : string;
-    attribute ASYNC_REG of data_reg : signal is "true";
-    attribute ASYNC_REG of data_o   : signal is "true";
+    attribute ASYNC_REG of data0_reg : signal is "true";
+    attribute ASYNC_REG of data1_reg : signal is "true";
 begin
     -- This is a simple two-stage delay line.
-    p_data_o : process(clk_i)
+    p_data_reg : process(clk_i)
     begin
         if rising_edge(clk_i) then
-            data_reg <= data_i;
-            data_o   <= data_reg;
+            data0_reg <= data_i;
+            data1_reg <= data0_reg;
         end if;
-    end process p_data_o;
+    end process p_data_reg;
+
+    data_o <= data1_reg;
 end Behavioral;
