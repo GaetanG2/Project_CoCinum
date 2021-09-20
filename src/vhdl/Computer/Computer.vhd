89e468f09d63b559d5865480d0da7e9d79156792
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-09-20 11:59:16 +0200

    Computer : version de base avec I/O et sans UART

--- src/vhdl/Computer/Computer.vhd
+++ src/vhdl/Computer/Computer.vhd
@@ -10,39 +10,32 @@ entity Computer is
     port(
         clk_i        : in  std_logic;
         btn_center_i : in  std_logic;
-        uart_tx_o    : out std_logic;
-        uart_rx_i    : in  std_logic
+        switches_i   : in  std_logic_vector(15 downto 0);
+        leds_o       : out std_logic_vector(15 downto 0)
     );
 end Computer;
 
 architecture Structural of Computer is
-    signal sync_reset     : std_logic;
-    signal sync_uart_rx   : std_logic;
+    signal sync_reset    : std_logic;
+    signal sync_switches : std_logic_vector(15 downto 0);
 
-    signal core_valid     : std_logic;
-    signal core_ready     : std_logic;
-    signal core_address   : word_t;
-    signal core_rdata     : word_t;
-    signal core_wdata     : word_t;
-    signal core_write     : std_logic_vector(3 downto 0);
-    signal core_irq       : std_logic;
+    signal core_valid    : std_logic;
+    signal core_ready    : std_logic;
+    signal core_address  : word_t;
+    signal core_rdata    : word_t;
+    signal core_wdata    : word_t;
+    signal core_write    : std_logic_vector(3 downto 0);
+    signal core_irq      : std_logic;
 
-    alias  dev_address    : byte_t is core_address(31 downto 24);
+    alias dev_address    : byte_t is core_address(31 downto 24);
 
-    signal mem_valid      : std_logic;
-    signal mem_ready      : std_logic;
-    signal mem_rdata      : word_t;
+    signal mem_valid     : std_logic;
+    signal mem_ready     : std_logic;
+    signal mem_rdata     : word_t;
 
-    signal intc_valid     : std_logic;
-    signal intc_ready     : std_logic;
-    signal intc_rdata     : word_t;
-    signal intc_events    : word_t;
-
-    signal uart_valid     : std_logic;
-    signal uart_ready     : std_logic;
-    signal uart_rdata     : word_t;
-    signal uart_tx_evt    : std_logic;
-    signal uart_rx_evt    : std_logic;
+    signal io_valid      : std_logic;
+    signal io_ready      : std_logic;
+    signal io_rdata      : word_t;
 begin
     -- Concurrent statements
 end Structural;
--- src/vhdl/Computer/Computer_pkg.vhd
+++ src/vhdl/Computer/Computer_pkg.vhd
@@ -7,17 +7,11 @@ use work.Virgule_pkg.all;
 
 package Computer_pkg is
 
-    constant CLK_FREQUENCY_HZ    : positive      := 100e6;
+    constant CLK_FREQUENCY_HZ : positive      := 100e6;
 
-    constant MEM_ADDRESS         : byte_t        := x"00";
-    constant MEM_CONTENT         : word_vector_t := work.Loader_pkg.DATA;
+    constant MEM_ADDRESS      : byte_t        := x"00";
+    constant MEM_CONTENT      : word_vector_t := work.SimpleIO_pkg.DATA;
 
-    constant INTC_ADDRESS        : byte_t        := x"81";
-
-    constant UART_ADDRESS        : byte_t        := x"82";
-    constant UART_BIT_RATE_HZ    : positive      := 115200;
-
-    constant INTC_EVENTS_UART_RX : natural       := 0;
-    constant INTC_EVENTS_UART_TX : natural       := 1;
+    constant IO_ADDRESS       : byte_t        := x"80";
 
 end Computer_pkg;
